cfa5998765fc7623b8b7aae81c3b690b61a7904a
George <gsims@softwareinstitute.com>
2022-04-11 17:32:45 +0100

    Started work on year

--- include/year.h
+++ include/year.h
@@ -1,45 +1,45 @@
-// #ifndef YEAR_H
-// #define YEAR_H
+#ifndef YEAR_H
+#define YEAR_H
 
-// #include "student.h"
-// #include "professor.h"
-// #include "moduleInstance.h"
-// #include <stdexcept>
-// #include <vector>
-// using std::vector;
-// using std::domain_error;
+#include "student.h"
+#include "professor.h"
+#include "moduleInstance.h"
+#include <stdexcept>
+#include <vector>
+using std::vector;
+using std::domain_error;
 
-// /**
-//  * @brief class that stores the available modules for this year, the enrolled students, professors and the active modules
-//  */
-// class year
-// {
-// private:
-//     unsigned int year_;
-//     vector<module_> modules;
-//     vector<student> students;
-//     vector<professor> professors;
-//     vector<moduleInstance> activeModules;
+/**
+ * @brief class that stores the available modules for this year, the enrolled students, professors and the active modules
+ */
+class year
+{
+private:
+    unsigned int year_;
+    vector<module_> *modules; //pointer to a module vector
+    vector<student*> students;//vector of student pointers
+    vector<professor*> professors;//vector of professor pointers
+    vector<moduleInstance> activeModules;//vector of module instances
 
-// public:
-//     year();
-//     year(year &year_);
-//     year(unsigned int year_, vector<module_> modules, vector<student> students, vector<professor> professors, vector<moduleInstance> activeModules);
-//     unsigned int getYear();
-//     void setYear(unsigned int year_);
-//     vector<student> &getStudents();
-//     void setStudents(vector<student> students);
-//     void addStudent(student student_);
-//     vector<module_> &getModule();
-//     void setModules(vector<module_> modules);
-//     void addModule(module_ module__);
-//     vector<professor> &getProfessors();
-//     void setProfessors(vector<professor> professors);
-//     void addProfessor(professor professor_);
-//     vector<moduleInstance> &getActiveModules();
-//     void setActiveModules(vector<moduleInstance> activeModules_);
-//     void addActiveModule(moduleInstance activeModule);
-//     moduleInstance &getActiveModule(string moduleCode) throw();
-// };
+public:
+    ~year();
+    year();
+    year(unsigned int year_, vector<module_> *modules, vector<student*> students, vector<professor*> professors, vector<moduleInstance> activeModules);
+    unsigned int getYear();
+    void setYear(unsigned int year_);
+    vector<student*> &getStudents();
+    void setStudents(vector<student*> students);
+    void addStudent(student *student_);
+    vector<module_> *getModule();
+    void setModules(vector<module_> *modules);
+    void addModule(module_ module__);
+    vector<professor*> &getProfessors();
+    void setProfessors(vector<professor*> professors);
+    void addProfessor(professor *professor_);
+    vector<moduleInstance> &getActiveModules();
+    void setActiveModules(vector<moduleInstance> activeModules_);
+    void addActiveModule(moduleInstance activeModule);
+    moduleInstance &getActiveModule(string moduleCode) throw();
+};
 
-// #endif
+#endif
--- src/year.cpp
+++ src/year.cpp
@@ -1,103 +1,104 @@
-// #include "../include/year.h"
+#include "../include/year.h"
 
-// year::year()
-// {
-//     this->year_ = 0u;
-//     this->modules = vector<module_>();
-//     this->students = vector<student>();
-//     this->professors = vector<professor>();
-//     this->activeModules = vector<moduleInstance>();
-// }
+year::~year(){
+    delete modules;
+}
 
-// year::year(unsigned int year_, vector<module_> modules, vector<student> students, vector<professor> professors, vector<moduleInstance> activeModules)
-// {
-//     this->year_ = year_;
-//     this->modules = modules;
-//     this->students = students;
-//     this->professors = professors;
-//     this->activeModules = activeModules;
-// }
+year::year()
+{
+    this->year_ = 0u;
+    this->modules = new vector<module_>();
+    this->students = vector<student*>();
+    this->professors = vector<professor*>();
+    this->activeModules = vector<moduleInstance>();
+}
 
-// year::year(year &year_) : year(year_.year_, year_.modules, year_.students, year_.professors, year_.activeModules)
-// {
-// }
+year::year(unsigned int year_, vector<module_> *modules, vector<student*> students, vector<professor*> professors, vector<moduleInstance> activeModules)
+{
+    this->year_ = year_;
+    this->modules = modules;
+    this->students = students;
+    this->professors = professors;
+    this->activeModules = activeModules;
+}
 
-// vector<student> &year::getStudents()
-// {
-//     return this->students;
-// }
+unsigned int year::getYear()
+{
+    return this->year_;
+}
 
-// void year::setStudents(vector<student> students)
-// {
-//     this->students = students;
-// }
+void year::setYear(unsigned int year_)
+{
+    this->year_ = year_;
+}
 
-// void year::addStudent(student student_)
-// {
-//     this->students.push_back(student_);
-// }
+vector<student *> &year::getStudents()
+{
+    return this->students;
+}
 
-// vector<module_> &year::getModule()
-// {
-//     return this->modules;
-// }
+void year::setStudents(vector<student *> students)
+{
+    this->students = students;
+}
 
-// void year::setModules(vector<module_> modules)
-// {
-//     this->modules = modules;
-// }
+void year::addStudent(student *student_)
+{
+    this->students.push_back(student_);
+}
 
-// void year::addModule(module_ module__)
-// {
-//     this->modules.push_back(module__);
-// }
+vector<module_> *year::getModule()
+{
+    return this->modules;
+}
 
-// vector<professor> &year::getProfessors()
-// {
-//     return this->professors;
-// }
+void year::setModules(vector<module_ > *modules)
+{
+    delete modules;
+    this->modules = modules;
+}
 
-// void year::setProfessors(vector<professor> professors)
-// {
-//     this->professors = professors;
-// }
+void year::addModule(module_ module__)
+{
+    this->modules->push_back(module__);
+}
 
-// void year::addProfessor(professor professor_)
-// {
-//     this->professors.push_back(professor_);
-// }
+vector<professor *> &year::getProfessors()
+{
+    return this->professors;
+}
 
-// vector<moduleInstance> &year::getActiveModules()
-// {
-//     return this->activeModules;
-// }
+void year::setProfessors(vector<professor *> professors)
+{
+    this->professors = professors;
+}
 
-// void year::setActiveModules(vector<moduleInstance> activeModules_)
-// {
-//     this->activeModules = activeModules_;
-// }
+void year::addProfessor(professor *professor_)
+{
+    this->professors.push_back(professor_);
+}
 
-// void year::addActiveModule(moduleInstance activeModule)
-// {
-//     this->activeModules.push_back(activeModule);
-// }
+vector<moduleInstance> &year::getActiveModules()
+{
+    return this->activeModules;
+}
 
-// moduleInstance &year::getActiveModule(string moduleCode) throw()
-// {
-//     for (auto &i : this->activeModules)
-//     {
-//         if (i.getModule().getModuleCode() == moduleCode)
-//         {
-//             return i;
-//         }
-//     }
-//     throw std::domain_error("No active module with that code");
-// }
+void year::setActiveModules(vector<moduleInstance> activeModules_)
+{
+    this->activeModules = activeModules_;
+}
 
-// unsigned int year::getYear(){
-//     return this->year_;
-// }
+void year::addActiveModule(moduleInstance activeModule)
+{
+    this->activeModules.push_back(activeModule);
+}
 
-// void year::setYear(unsigned int year_){
-//     this->year_ = year_;
-// }
+moduleInstance &year::getActiveModule(string moduleCode) throw()
+{
+    for(auto &i : this->activeModules){
+        if(i.getModule().getModuleCode() == moduleCode){
+            return i;
+        }
+    }
+    throw std::domain_error("No such active module");
+}
