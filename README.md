630390ef84cca12bce6b65756d0219c7b3d9a491
George <gsims@softwareinstitute.com>
2022-04-12 12:19:49 +0100

    Started planning out app

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
 11/04/2022 
         - Did a lot of work towards keeping objects as reference's but it caused a lot of issues with inheritance, will revisit with time and do it with pointers
         - just learnt about circular dependency and how its been causing certain issues so I need to keep that in mind for future 
-
-
-** Need to finish and fix student methods for getModules(), currently throwing an error when creating a moduleInstance vector **
+12/04/2022
+        - tested and fixed student module instance and module
+        - finished up and tested year
+        - tested and fixed system, had issue with modifying passed values
+        - start planning out app
 
 ** practice copy constructors as my attempt seems to break code **
+
+**to do**
+* display methods
+* add methods
+* modify methods
+* run method
--- include/app.h
+++ include/app.h
@@ -4,16 +4,113 @@
 #include "system.h"
 #include "userInput.h"
 
+#include <iostream>
+using std::cout;
+using std::endl;
+
 /**
  * @brief the main application that handles user interaction and inputs 
+ * 
+ * modify or add flow: * 
+ * student -> person -> address
+ * professor -> person -> address
+ * year -> module
+ * year -> moduleInstance
+ * year -> moduleInstance -> assignment
  */
-// class app{
-//     private:
-//         uniSystem sys;
-//     public:
-//     app();
-//     app(uniSystem &sys);
-//     void run(); //I am doing this last cause fuck anything else
-// };
+class app{
+    private:
+        uniSystem sys;
+        year * currentYear;
+        module_ * currentModule;
+        moduleInstance * currentModuleInstance;
+        student * currentStudent;
+        professor * currentProfessor;
+
+    public:
+    app();
+    app(uniSystem &sys);
+
+    /**
+     * @brief responsible for adding new students, module or moduleinstances to a year
+     */
+    void modifyYear();
+
+    /**
+     * @brief Take inputs required to add a new year 
+     */
+    void addYear();
+
+    /**
+     * @brief responsible for modifying a students details e.g. change name or address 
+     */
+    void modifyStudent();
+
+    /**
+     * @brief  Take inputs required to create a new student
+     */
+    void addStudent();
+
+    /**
+     * @brief responsible for modifying a professor
+     */
+    void modifyProfessor();
+
+    /**
+     * @brief Take inputs required to create a new professor 
+     */
+    void addProfessor();
+
+    /**
+     * @brief method to modify a person e.g. student or professor 
+     */
+    void modifyPerson(person * currentPerson);
+
+    /**
+     * @brief Take inputs required to make a new person 
+     * @return person instance to create a derived class e.g. student or professor
+     */
+    person addPerson();
+
+    /**
+     * @brief modify module variables, this only effects future instances 
+     */
+    void modifyModule();
+
+    /**
+     * @brief Take inputs to create a module
+     */
+    void addModule();
+
+    /**
+     * @brief modify a module instance e.g. giving student grades 
+     */
+    void modifyModuleInstance();
+
+    /**
+     * @brief take inputs to create a new module instance 
+     */
+    void addModuleInstance();
+
+    /**
+     * @brief method to modify an assignmet 
+     */
+    void modifyAssignment(); // e.g. giving grades
+
+    /**
+     * @brief utility functions to print out details of the years and students
+     */
+    void displayStudents();
+    void displayStudent(unsigned int studentNum); // student num firstName lastName gpa
+    void displayProfessors();
+    void displayProfessor(unsigned int staffNum); // staff num firstName lastName position
+    void displayYears();
+    void displayYear(unsigned int year);// year numberOfStudents numberOfActiveModules
+    void displayModuleInstance(string code); // {code = year+modulecode}year moduleCode display assignment
+    void displayAssignment(string code); //code + desc 
+
+
+    void run();
+};
 
 #endif
